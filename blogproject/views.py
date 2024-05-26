204fff08da41727245097b4a9df74bc0ae95f258
Anonymous <isurusameera88@gmail.com>
2024-05-26 13:43:59 +0530

    add two templates and reneder them from views

--- blogproject/views.py
+++ blogproject/views.py
@@ -1,7 +1,7 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("Hello world")
+    return render(request,"home.html")
 
 def about(request):
-    return HttpResponse("about page")
+    return render(request,"about.html")
